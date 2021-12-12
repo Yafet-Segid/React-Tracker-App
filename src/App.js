980d6dc3c172626eb2c95c31404d95c8e0aa162d
yafet segid <yafetsegid@gmail.com>
2021-12-12 15:14:42 -0600

    adding API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const fetchRundomData = () => {
+  return axios
+    .get("https://randomuser.me/api/")
+    .then((data) => {
+      // handle success
+      console.log(data);
+      return JSON.stringify(data);
+    })
+    .catch((err) => {
+      // handle error
+      console.log(err);
+    });
+};
 
 function App() {
-  const [counter, setcounter] = useState(0);
+  // const [randonData, setRandonData] = useState();
+  const [jsonRandonData, setjsonRandonData] = useState();
+
+  useEffect(() => {
+    fetchRundomData().then((randomData) => {
+      setjsonRandonData(randomData || "No user data found");
+    });
+  }, []);
 
   return (
-    <div>
-      <p>{counter}</p>
+    <div className="App">
       <button
         onClick={() => {
-          var number = 1;
-          setcounter(counter + number);
+          fetchRundomData();
         }}
       >
-        Add Yes
+        Add
       </button>
+      <p>{jsonRandonData}</p>
     </div>
   );
 }
